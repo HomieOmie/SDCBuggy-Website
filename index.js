09824729c62e9a925db31b9f009919afabe506d1
HomieOmie <opatel@andrew.cmu.edu>
2023-07-24 20:18:04 -0400

    Navbar skeleton finished

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-})
+    navbar.classList.toggle("active");
+});
