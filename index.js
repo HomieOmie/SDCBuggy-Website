a5d643086de36d73bc1cd9d015257576af88de5d
Om Patel <opatel@andrew.cmu.edu>
2023-08-12 11:28:06 -0400

    Alot of changes made

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const main2Section = document.querySelector(".main2");
     const switchButtonBackground = document.querySelector(".scroll-button-background");
 
+    let currentSlideIndex = 0;
+
     switchButton.addEventListener("click", function () {
         if (main1Section.getBoundingClientRect().top < 0) {
             // main1Section.scrollIntoView({ behavior: "smooth" });
@@ -41,4 +43,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
+    const malice_front_sunny = {
+        "image-url": "../images/media/malice-front-sunny.jpg"
+    };
+
+    const bane_front_hill2 = {
+        "image-url": "../images/media/bane-front-hill2.jpg"
+    };
+
+    const design_comp_group_12 = {
+        "image-url": "../images/media/design-comp-group-12.jpg"
+    };
+
+    const vice_side_misty = {
+        "image-url": "../images/media/vice-side-misty.jpg"
+    };
+
+    const bow_to_bane = {
+        "image-url": "../images/media/bow-to-bane.jpg"
+    };
+
+    const vice_hill5_incoming = {
+        "image-url": "../images/media/vice-hill5-incoming.jpg"
+    };
+
+    const slides = [malice_front_sunny, bane_front_hill2, design_comp_group_12,
+                    vice_side_misty, bow_to_bane, vice_hill5_incoming]
+
+    function showSlide(index) {
+        console.log(slides[index]["image-url"], slides[index]["properties"]);
+        main1Section.style.backgroundImage = `url(${slides[index]["image-url"]})`;
+    }
+    
+    function nextSlide() {
+        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+        showSlide(currentSlideIndex);
+    }
+    
+      // Start the slideshow
+    showSlide(currentSlideIndex);
+    setInterval(nextSlide, 2000);
 });
