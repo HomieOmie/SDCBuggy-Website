20162c6ef8f05143fc5415d7036bf370d65e43ed
Om Patel <opatel@andrew.cmu.edu>
2023-11-14 23:13:30 -0500

    Re-worked v1 main page; need to work on making code organized and adding colors

--- scripts/body/bodyConstants.js
+++
@@ -1,4 +0,0 @@
-export const switchButton = document.getElementById("carrotImage");
-export const main1Section = document.querySelector(".main1");
-export const main2Section = document.querySelector(".main2");
-export const switchButtonBackground = document.querySelector(".scroll-button-background");
--- scripts/header/navbar/navbarConstants.js
+++
@@ -1,2 +0,0 @@
-export const hamburger = document.querySelector(".hamburger");
-export const navMenu = document.querySelector(".nav-menu");
