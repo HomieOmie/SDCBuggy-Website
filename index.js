a87899ebe1401cd4c48be506fe8a05c03b8e6b87
Om Patel <opatel@andrew.cmu.edu>
2023-12-26 16:06:41 -0500

    Added open and close leaderhsip card functionality; now working on contents

--- index.js
+++ index.js
@@ -5,3 +5,39 @@ function myFunction() {
     var mobileMenu = document.getElementById("mobileMenu");
     mobileMenu.style.display = (mobileMenu.style.display === 'flex') ? 'none' : 'flex';
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('click', function () {
+            cards.forEach(otherCard => {
+                if (otherCard !== card) {
+                    otherCard.style.display = 'none';
+                }
+            })
+
+            const closeButton = card.querySelector('.close');
+
+            card.style.width = '92vw';
+            card.style.minHeight = '76vh';
+            card.style.backgroundColor = `#33333377`;
+            card.style.marginRight = '0vh';
+
+            closeButton.style.display = `flex`;
+
+            closeButton.addEventListener('click', function (event) {
+                card.style.width = 'auto';
+                card.style.minHeight = 'auto';
+                card.style.backgroundColor = '#ffffff';
+                card.style.marginRight = '5vh';
+                closeButton.style.display = 'none';
+
+                cards.forEach(otherCard => {
+                    otherCard.style.display = 'flex';
+                });
+
+                event.stopPropagation();
+            })
+        })
+    })
+})
