5e85843379d6293a972b90c45234b252002b16a9
Om Patel <opatel@andrew.cmu.edu>
2024-01-16 01:50:48 -0500

    Reworking leads section to be more factorable

--- index.js
+++ index.js
@@ -10,72 +10,91 @@ function myFunction() {
     mobileMenu.style.display = (mobileMenu.style.display === 'flex') ? 'none' : 'flex';
 }
 
-// For the leads
 document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => {
         card.addEventListener('click', function () {
             cards.forEach(otherCard => {
-                if (otherCard !== card) {
-                    otherCard.style.display = 'none';
+                if (otherCard != card) {
+                    otherCard.classList.toggle('hidden');
                 }
             })
 
-            const closeButton = card.querySelector('.close');
-            const circle = card.querySelector('.circle');
-            const pTags = card.querySelectorAll('p');
-            const name = pTags[0];
-            const title = pTags[1];
-            const info = card.querySelector('.info');
-            const links = card.querySelector('.links');
+            card.classList.toggle('card-expanded');
 
-            card.style.width = '92vw';
-            card.style.minHeight = '76vh';
-            card.style.backgroundColor = `#33333377`;
-            card.style.marginRight = '0vh';
+            const closeButton = card.querySelector('close');
+            closeButton.classList.toggle('close-expanded')
 
-            name.style.fontSize = '4vh';
-            name.style.fontFamily = 'sora-semibold';
-            title.style.fontSize = '4vh';
-            title.style.fontFamily = 'sora-semibold';
-            circle.style.height = '30vh';
-            circle.style.width = '30vh';
-
-            info.style.display = 'flex';
-
-            links.style.display = 'flex';
-
-            closeButton.style.display = `flex`;
-
-            closeButton.addEventListener('click', function (event) {
-                card.style.width = 'auto';
-                card.style.minHeight = 'auto';
-                card.style.backgroundColor = '#000000';
-                card.style.marginRight = '5vh';
-
-                name.style.fontFamily = 'sora-regular';
-                name.style.fontSize = '2vh';
-                title.style.fontFamily = 'sora-regular';
-                title.style.fontSize = '2vh';
-
-                circle.style.height = '16vh';
-                circle.style.width = '16vh';
-
-                info.style.display = 'none';
-
-                links.style.display = 'none';
-
-                closeButton.style.display = 'none';
-
-                cards.forEach(otherCard => {
-                    otherCard.style.display = 'flex';
-                });
-
-                event.stopPropagation();
-            })
         })
     })
 })
+
+// For the leads
+// document.addEventListener('DOMContentLoaded', function () {
+//     const cards = document.querySelectorAll('.card');
+//     cards.forEach(card => {
+//         card.addEventListener('click', function () {
+//             cards.forEach(otherCard => {
+//                 if (otherCard !== card) {
+//                     otherCard.style.display = 'none';
+//                 }
+//             })
+
+//             const closeButton = card.querySelector('.close');
+//             const circle = card.querySelector('.circle');
+//             const pTags = card.querySelectorAll('p');
+//             const name = pTags[0];
+//             const title = pTags[1];
+//             const info = card.querySelector('.info');
+//             const links = card.querySelector('.links');
+
+//             card.style.width = '92vw';
+//             card.style.minHeight = '76vh';
+//             card.style.backgroundColor = `#33333377`;
+//             card.style.marginRight = '0vh';
+
+//             name.style.fontSize = '4vh';
+//             name.style.fontFamily = 'sora-semibold';
+//             title.style.fontSize = '4vh';
+//             title.style.fontFamily = 'sora-semibold';
+//             circle.style.height = '30vh';
+//             circle.style.width = '30vh';
+
+//             info.style.display = 'flex';
+
+//             links.style.display = 'flex';
+
+//             closeButton.style.display = `flex`;
+
+//             closeButton.addEventListener('click', function (event) {
+//                 card.style.width = 'auto';
+//                 card.style.minHeight = 'auto';
+//                 card.style.backgroundColor = '#000000';
+//                 card.style.marginRight = '5vh';
+
+//                 name.style.fontFamily = 'sora-regular';
+//                 name.style.fontSize = '2vh';
+//                 title.style.fontFamily = 'sora-regular';
+//                 title.style.fontSize = '2vh';
+
+//                 circle.style.height = '16vh';
+//                 circle.style.width = '16vh';
+
+//                 info.style.display = 'none';
+
+//                 links.style.display = 'none';
+
+//                 closeButton.style.display = 'none';
+
+//                 cards.forEach(otherCard => {
+//                     otherCard.style.display = 'flex';
+//                 });
+
+//                 event.stopPropagation();
+//             })
+//         })
+//     })
+// })
 
 // For the roles cards section
 document.addEventListener('DOMContentLoaded', function () {
